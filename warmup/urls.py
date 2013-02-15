e8c6d2cb0024b6b5138dbe1a9913e261ec5d6769
flawedmatrix <flawedmatrix@gmail.com>
2013-02-15 11:20:20 -0800

    Fixing static file serving

--- warmup/urls.py
+++ warmup/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,5 +19,5 @@ urlpatterns = patterns('',
     url(r'^TESTAPI/resetFixture$', 'users.views.TESTAPI_resetFixture'),
     url(r'^TESTAPI/unitTests$', 'users.views.TESTAPI_unitTests'),
     url(r'^(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '/home/flawedmatrix/Desktop/projects/warmup/static'}),
+        {'document_root': settings.STATIC_ROOT}),
 )
